f8ff1745a21e6b3d96e7447c63cde0f0a870b34f
pknatictech <pknatic@gmail.com>
2023-11-22 10:59:59 +0530

    update

--- LetUsC/Research/research_1.c
+++ LetUsC/Research/research_1.c
@@ -1,55 +1,38 @@
-// // 7 7 7 7 7 7 7 7 7 7 7 7 7
-// // 7 6 6 6 6 6 6 6 6 6 6 6 7
-// // 7 6 5 5 5 5 5 5 5 5 5 6 7
-// // 7 6 5 4 4 4 4 4 4 4 5 6 7
-// // 7 6 5 4 3 3 3 3 3 4 5 6 7
-// // 7 6 5 4 3 2 2 2 3 4 5 6 7
-// // 7 6 5 4 3 2 1 2 3 4 5 6 7
-// // 7 6 5 4 3 2 2 2 3 4 5 6 7
-// // 7 6 5 4 3 3 3 3 3 4 5 6 7
-// // 7 6 5 4 4 4 4 4 4 4 5 6 7
-// // 7 6 5 5 5 5 5 5 5 5 5 6 7
-// // 7 6 6 6 6 6 6 6 6 6 6 6 7
-// // 7 7 7 7 7 7 7 7 7 7 7 7 7
-// #include<stdio.h>
-// int main()
-// {
-
-//     int i,j;
-//     int n=7,p=n;
-//     // scanf("%d", &n);
-//   	// Complete the code to print the pattern.
-//     for(i=1;i<=n*2-1;i++)
-//     {
-//         for(j=1;j<=n*2-1;j++)
-//         {
-//             printf("%d ",p);
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
-
-
+// 7 7 7 7 7 7 7 7 7 7 7 7 7
+// 7 6 6 6 6 6 6 6 6 6 6 6 7
+// 7 6 5 5 5 5 5 5 5 5 5 6 7
+// 7 6 5 4 4 4 4 4 4 4 5 6 7
+// 7 6 5 4 3 3 3 3 3 4 5 6 7
+// 7 6 5 4 3 2 2 2 3 4 5 6 7
+// 7 6 5 4 3 2 1 2 3 4 5 6 7
+// 7 6 5 4 3 2 2 2 3 4 5 6 7
+// 7 6 5 4 3 3 3 3 3 4 5 6 7
+// 7 6 5 4 4 4 4 4 4 4 5 6 7
+// 7 6 5 5 5 5 5 5 5 5 5 6 7
+// 7 6 6 6 6 6 6 6 6 6 6 6 7
+// 7 7 7 7 7 7 7 7 7 7 7 7 7
 #include <stdio.h>
 
 void printPattern(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      if (i == 1 || i == n || j == 1 || j == n) {
-        printf("*");
-      } else {
-        printf(" ");
-      }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int min = (i < j) ? i : j;
+            int value = n - min + 1;
+            printf("%d ", value);
+        }
+        for (int j = n - 1; j >= 1; j--) {
+            int min = (i < j) ? i : j;
+            int value = n - min + 1;
+            printf("%d ", value);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
 
 int main() {
-  int n;
-  printf("Enter the number of rows: ");
-  scanf("%d", &n);
-  printPattern(n);
-  return 0;
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    printPattern(n);
+    return 0;
 }
