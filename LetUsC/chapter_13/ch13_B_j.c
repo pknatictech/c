362fc2043ad19ac31de5a79d510bb7e77a312706
pknatictech <pknatic@gmail.com>
2023-12-05 23:06:23 +0530

    update

--- LetUsC/chapter_13/ch13_B_j.c
+++ LetUsC/chapter_13/ch13_B_j.c
@@ -2,72 +2,131 @@
 A dequeue is an ordered set of elements in which elements may be inserted or retrieved from either end. using an array simulate a dequeue of characters and the operation retrieve left,retrieve right, insert left, insert right. Exceptional conditions such as dequeue full of empty should be reported. use two pointers left and right for this simulation.
 */
 #include <stdio.h>
-#define MAX 20
-int i = 0;
-int add_front(int *arr);
+#include <stdlib.h>
+#define MAX 5
+void lAdd(int *lside, char *q, int *sizeq, char ch);
+void rAdd(int *lside, char *q, int *sizeq, char ch);
+void show(char *q, int *sizeq);
+void rDel(int *lside, char *q, int *sizeq);
+void lDel(int *lside, char *q, int *sizeq);
+
 int main()
 {
-    static int arr[MAX];
-    int option, loop=1;
-    printf("Select Option From menu.");
-    while (loop == 1)
+    char que[MAX];
+    int left, size, x;
+    left = size = -1;
+    char ch;
+    while (1)
     {
-        printf("\n 1. Enter the element in Queue from front\n 2. Enter the element in Queue from end \n 3. Delete the element from front.\n 4. Delete the item from end. \n 5. Display the Dequeue. \n 6. Exit.\n");
-        scanf("%d", &option);
-        switch (option)
+        printf("Choose the option.\n");
+        printf("1. insert data form Right\n");
+        printf("2. insert data from Left\n");
+        printf("3. Delete data from Right\n");
+        printf("4. Delete data from Left\n");
+        printf("5. Display Menu\n");
+        printf("6. Exit Program\n==>: ");
+        scanf("%d", &x);
+        switch (x)
         {
         case 1:
-        {
-            int auto a,temp; 
-            add_front(arr+i);
-            printf("%d",arr[i]);
-            i++;
+            getchar();
+            printf("Enter the character ");
+            scanf("%c", &ch);
+            rAdd(&left, que, &size, ch);
+            printf("Character entered Successfully!\n\n");
             break;
-        }
         case 2:
-        {
-            printf(" We are in 1st case ");
+            getchar();
+            printf("Enter the character ");
+            scanf("%c", &ch);
+            lAdd(&left, que, &size, ch);
+            printf("Character entered Successfully!\n\n");
             break;
-        }
         case 3:
-        {
-            printf(" We are in 1st case ");
+            rDel(&left, que, &size);
+            printf("Character Delete Successfully!\n\n");
             break;
-        }
         case 4:
-        {
-            printf(" We are in 1st case ");
+            lDel(&left, que, &size);
+            printf("Character Delete Successfully!\n\n");
             break;
-        }
         case 5:
-        {
-            printf("[");
-            for(int a = 0 ;a < i;a++)
-            {
-                printf("%d,",arr[a]);
-            }
-            printf("]");
+            show(que, &size);
             break;
-        }
         case 6:
-        {
-            printf(" Nice to work with you Come Again  ");
-            loop = 0;
+            exit(1);
             break;
-        }
-        default:
-        {
-            printf("You enterred wrong entry please enter a valid number");
-            
-        }
         }
     }
     return 0;
 }
-int add_front(int *p)
+
+void rAdd(int *lside, char *q, int *sizeq, char ch)
 {
-    printf("Enter the value You want to add  ");
-    scanf("%d",p);
+    if (*sizeq == MAX - 1)
+    {
+        printf("\nDequeue is full, no more insertion is possible.\n");
+        return;
+    }
+
+    if (*sizeq == -1)
+        *sizeq = 0;
+    else
+    {
+        *sizeq = *sizeq + 1;
+    }
+    q[*sizeq] = ch;
 }
 
-//Pending..........................................................................................
+void lAdd(int *lside, char *q, int *sizeq, char ch)
+{
+    int i;
+    if (*sizeq == MAX - 1)
+    {
+        printf("\nDequeue is full, no more insertion is possible.\n");
+        return;
+    }
+
+    for (i = *sizeq; i >= 0; i--)
+        q[i + 1] = q[i];
+
+    q[0] = ch;
+
+    if (*lside == -1)
+    {
+        *lside = 0;
+    }
+    *sizeq = *sizeq + 1;
+}
+
+void show(char *q, int *sizeq)
+{
+    int i;
+    printf("\n\nList\n");
+    for (i = 0; i <= *sizeq; i++)
+        printf("%c ", q[i]);
+    printf("\n\n");
+}
+
+void lDel(int *lside, char *q, int *sizeq)
+{
+    int item = q[0], i;
+    if (*sizeq == -1)
+    {
+        printf("\nList is empty.\n");
+    }
+
+    for (i = 0; i < *sizeq; i++)
+        q[i] = q[i + 1];
+
+    *sizeq = *sizeq - 1;
+}
+
+void rDel(int *lside, char *q, int *sizeq)
+{
+    if (*sizeq == -1)
+    {
+        printf("\nList is empty.\n");
+    }
+    *sizeq = *sizeq - 1;
+}
