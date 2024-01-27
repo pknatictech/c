77d8f19ef78498493881c221c5f0a27316a9a3c2
pknatic <pknatic@gmail.com>
2024-01-27 07:03:33 +0530

    update

--- LetUsC/Research/tempCodeRunnerFile.c
+++ LetUsC/Research/tempCodeRunnerFile.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#define getchh() scanf("%c",&ch);
+
 int main()
 {
-    printf("What do you want ?");
+    char ch;
+    char str[10];
+    printf("Enter the character ");
+    // scanf("%c",&ch);
+    // ch = getchar();
+    // putchar(ch);
+    // gets(str);
+    // puts(str);
+    getchh();
+    printf("%c",ch);
     return 0;
 }
