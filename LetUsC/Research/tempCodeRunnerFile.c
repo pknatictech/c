5fca564cb29d95a38e600ce9d6ba6232e042ee97
pknatic <pknatic@gmail.com>
2024-01-30 18:22:14 +0530

    update

--- LetUsC/Research/tempCodeRunnerFile.c
+++ LetUsC/Research/tempCodeRunnerFile.c
@@ -1,17 +1,30 @@
 #include<stdio.h>
-#define getchh() scanf("%c",&ch);
-
+#include<stdlib.h>
 int main()
 {
     char ch;
-    char str[10];
-    printf("Enter the character ");
-    // scanf("%c",&ch);
-    // ch = getchar();
-    // putchar(ch);
-    // gets(str);
-    // puts(str);
-    getchh();
-    printf("%c",ch);
+    FILE *p,*c;
+    p = fopen("mynote.txt","r");
+    if(p==NULL)
+    {
+        printf("can not find the file to open.");
+        exit(1);
+    }
+    c = fopen("pk.txt","w");
+    if(c==NULL)
+    {
+        printf("can not create or open the file to write.");
+        exit(2);
+    }
+    while(1)
+    {
+        ch = fgetc(p);
+        if(ch==EOF)
+        break;
+        else
+        fputc(ch,c);
+    }
+    fclose(p);
+    fclose(c);
     return 0;
 }
